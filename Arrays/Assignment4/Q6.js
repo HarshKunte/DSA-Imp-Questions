80a81844d42256259e97fd783bdb1acf6fca374d
Harsh Kunte <harshkunte99@gmail.com>
2023-06-04 00:47:40 +0530

    assignment 4 q6 updated

--- Arrays/Assignment4/Q6.js
+++ Arrays/Assignment4/Q6.js
@@ -1,5 +1,17 @@
 function sortedSquares(nums) {
-    const squaredNums = nums.map(num => num * num);
-    squaredNums.sort((a, b) => a - b);
-    return squaredNums;
+  let start = 0;
+  let end = nums.length - 1;
+  let idx = end;
+  let sortPowArr = [];
+
+  while (idx > -1) {
+      if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+          sortPowArr[idx--] = nums[start] * nums[start];
+          start++;
+      } else {
+          sortPowArr[idx--] = nums[end] * nums[end];
+          end--;
+      }
+  }
+  return sortPowArr;
   }
